45b03adbf82c7ed076f53e58ffd79695f128e49f
Ariyan3256 <116423514+Ariyan3256@users.noreply.github.com>
2023-05-30 19:05:34 +0530

    dd

--- recipe.js
+++ recipe.js
@@ -13,10 +13,162 @@ function showMsg(){
     const dishName=dish.value;
     if(dishName=='chicken'){
         window.open('chicken.html','newwindow');
+        let p=fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772");
+        p.then((response)=>{
+            console.log(response.status);
+            console.log()
+            return response.json()
+        }).then((completedata)=>{
+            //console.log(values);
+            let data1="";
+            completedata.map((values)=>{
+                data1+= `<div class="card">
+                <h1 class="title">${values.meals}</h1>
+                //<img src="" alt="img" class="images">
+                <p></p>
+                <p></p>
+                <p></p>
+                console.log(${values.meals});
+            </div>`
+            })
+        }).catch((err)=>{
+            console.log(err);
+        })
+        let q=fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+        q.then((response)=>{
+            console.log(response.status);
+            console.log()
+            return response.json()
+        }).then((completedata)=>{
+            console.log(completedata)
+            //console.log(values);
+            let data1="";
+            completedata.map((values)=>{
+                data1+= `<div class="card">
+                <h1 class="title">${values.meals}</h1>
+                //<img src="" alt="img" class="images">
+                <p></p>
+                <p></p>
+                <p></p>
+                console.log(${values.categories[2]});
+            </div>`
+            })
+        }).catch((err)=>{
+            console.log(err);
+        })
     }
     else if(dishName=='dessert'){
         window.open('dessert.html','newwindow');
+        let p=fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a");
+        p.then((response)=>{
+            console.log(response.status);
+            console.log()
+            return response.json()
+        }).then((completedata)=>{
+            //console.log(values);
+            let data1="";
+            completedata.map((values)=>{
+                data1+= `<div class="card">
+                <h1 class="title">${values.meals}</h1>
+                //<img src="" alt="img" class="images">
+                <p></p>
+                <p></p>
+                <p></p>
+                console.log(${values.meals});
+            </div>`
+            })
+        }).catch((err)=>{
+            console.log(err);
+        })
+        let q=fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+        q.then((response)=>{
+            console.log(response.status);
+            console.log()
+            return response.json()
+        }).then((completedata)=>{
+            //console.log(values);
+            let data1="";
+            completedata.map((values)=>{
+                data1+= `<div class="card">
+                <h1 class="title">${values.meals}</h1>
+                //<img src="" alt="img" class="images">
+                <p></p>
+                <p></p>
+                <p></p>
+                console.log(${values.meals});
+            </div>`
+            })
+        }).catch((err)=>{
+            console.log(err);
+        })
+    }
+    else if(dishname=='vegeterian'){
+        let p=fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata");
+        p.then((response)=>{
+            console.log(response.status);
+            console.log()
+            return response.json()
+        }).then((completedata)=>{
+            //console.log(values);
+            let data1="";
+            completedata.map((values)=>{
+                data1+= `<div class="card">
+                <h1 class="title">${values.meals}</h1>
+                //<img src="" alt="img" class="images">
+                <p></p>
+                <p></p>
+                <p></p>
+                console.log(${values.meals});
+            </div>`
+            })
+        }).catch((err)=>{
+            console.log(err);
+        })
+        let q=fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
+        q.then((response)=>{
+            console.log(response.status);
+            console.log()
+            return response.json()
+        }).then((completedata)=>{
+            //console.log(values);
+            let data1="";
+            completedata.map((values)=>{
+                data1+= `<div class="card">
+                <h1 class="title">${values.meals}</h1>
+                //<img src="" alt="img" class="images">
+                <p></p>
+                <p></p>
+                <p></p>
+                console.log(${values.categories[2]});
+            </div>`
+            })
+        }).catch((err)=>{
+            console.log(err);
+        })
     }
     console.log();
     alert(dishName);
 }
+
+
+let p=fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata");
+p.then((response)=>{
+    console.log(response.status);
+    console.log()
+    return response.json()
+}).then((completedata)=>{
+    console.log(completedata);
+    let data1="";
+    completedata.map((values)=>{
+        data1+= `<div class="card">
+        <h1 class="title">${values.meals}</h1>
+        //<img src="" alt="img" class="images">
+        <p></p>
+        <p></p>
+        <p></p>
+        console.log(${values.meals});
+    </div>`
+    })
+}).catch((err)=>{
+    console.log(err);
+})
